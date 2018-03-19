7e7a48be872b13c6bf5a109287194a0fb3a9becf
Kate Ford <katefwannell7@gmail.com>
2018-03-19 14:56:21 -0700

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Simple Calculator</title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h1 class="auto-style1">Simple Calculator</h1>
+    <div>
+    
+        <asp:Label ID="firstValueLabel" runat="server" Text="First Value:" OnDataBinding="Page_Load" OnLoad="Page_Load"></asp:Label>
+&nbsp;<asp:TextBox ID="firstValueTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="secondValueLabel" runat="server" Text="Second Value:"></asp:Label>
+&nbsp;<asp:TextBox ID="secondValueTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" Text="+" OnClick="addButton_Click" />
+&nbsp;
+        <asp:Button ID="subtractButton" runat="server" Text="-" OnClick="subtractButton_Click" />
+&nbsp;
+        <asp:Button ID="multiplyButton" runat="server" Text="*" OnClick="multiplyButton_Click" />
+&nbsp;
+        <asp:Button ID="divisionButton" runat="server" Text="/" OnClick="divisionButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server" BackColor="#6699FF" Font-Bold="True"></asp:Label>
+    
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // This can be done on the toolbar but this is how you would do it at runtime ...
+            firstValueLabel.Font.Name = "arial";
+            firstValueLabel.Font.Size = 14;
+            secondValueLabel.Font.Name = "arial";
+            secondValueLabel.Font.Size = 14;
+            resultLabel.Font.Name = "arial";
+            resultLabel.Font.Size = 14;
+        }
+         
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            int firstValueInt = int.Parse(firstValueTextBox.Text);
+            int secondValueInt = int.Parse(secondValueTextBox.Text);
+            resultLabel.Text = (firstValueInt + secondValueInt).ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            int firstValueInt = int.Parse(firstValueTextBox.Text);
+            int secondValueInt = int.Parse(secondValueTextBox.Text);
+            resultLabel.Text = (firstValueInt - secondValueInt).ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            int firstValueInt = int.Parse(firstValueTextBox.Text);
+            int secondValueInt = int.Parse(secondValueTextBox.Text);
+
+            // Raising an error for overflow when doing multiplication
+            long resultMultiplicationLong;
+  
+            checked
+            {
+                resultMultiplicationLong = firstValueInt * secondValueInt;
+            }
+
+            resultLabel.Text = resultMultiplicationLong.ToString();
+
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            // Implicit Upcasting to a double so that precision isn't lost ...
+            double firstValueDouble = int.Parse(firstValueTextBox.Text);
+            double secondValueDouble = int.Parse(secondValueTextBox.Text);
+            double divisionResultDouble = firstValueDouble/secondValueDouble;
+
+            resultLabel.Text = divisionResultDouble.ToString();
+
+        }
+
+    }
+}
